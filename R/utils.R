0cbc04014eb0867d055e7ee2bdc79cc430ea3797
Kenneth Cheng <cheng.ky@hotmail.com>
2019-10-18 00:25:27 -0400

    minor fix get.diff.comb.flux

--- R/utils.R
+++ R/utils.R
@@ -424,7 +424,8 @@ get.diff.comb.flux <- function(imat.model0, imat.model1, use.sample=TRUE, sample
     m0 <- (ub0+lb0)/2
     m1 <- (ub1+lb1)/2
     dm <- m1-m0
-    res <- data.table(id=ifelse(is.null(names(rxns)), 1:length(rxns), names(rxns)),
+    if (is.null(names(rxns))) id <- 1:length(rxns) else id <- names(rxns)
+    res <- data.table(id=id,
                       lb0=lb0, ub0=ub0, med0=m0,
                       lb1=lb1, ub1=ub1, med1=m1,
                       diff.med=dm)
